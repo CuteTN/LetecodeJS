78290b2241e08a256a209aadea9a24d928d304ed
CuteTN <quantiennghia001@gmail.com>
2022-07-28 09:19:38 +0700

    Handle default problem ID

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import { runTests } from './core/autotest.js'
+import { DEFAULT_PROBLEM_ID } from './core/constants/defaults.js';
 import { kitsCore } from './core/globals.js';
+import { openVsCode, runSetUp } from './core/setup.js';
 
 function main() {
   console.clear();
@@ -16,16 +18,9 @@ function main() {
   }
   let { problemId } = currentProblem ?? {} 
 
-  if (!problemId) {
-    console.error(`The current problem ID is not provided.`);
-    console.error(`Please run "npm run problem <your-problem-id>" to initialize your new solution!`);
-    return;
-  }
-
-  if (!fs.existsSync(`./problems/${problemId}`)) {
-    console.error(`The solution folder for problem ${problemId} does not exist.`);
-    console.error(`Please run "npm run problem ${problemId}" to initialize your new solution!`);
-    return;
+  if (!(problemId && fs.existsSync(`./problems/${problemId}`))) {
+    runSetUp(DEFAULT_PROBLEM_ID);
+    openVsCode(DEFAULT_PROBLEM_ID);
   }
   
   Promise.all([
@@ -34,7 +29,7 @@ function main() {
     import(`./problems/${problemId}/testcases.js`),
   ])
     .then(([configs, solution, { testcases }]) => {
-      // console.clear();
+      console.clear();
       kitsCore.executeTasks();
       runTests(problemId, solution.default, testcases, configs)
     })
